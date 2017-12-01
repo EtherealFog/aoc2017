ed074a6a7876e1f305e3aaa64f4c7aa5b0737eb5
EtherealFog <baldwinelias77@gmail.com>
2017-12-01 12:55:52 -0800

    added another way of solving

--- day1/Day1.java
+++ day1/Day1.java
@@ -6,6 +6,7 @@ public class Day1 {
       Scanner input = new Scanner(new File("input.txt"));
       String in = input.next();
       a(in);
+      a8(in);
       b(in);
    }
 
@@ -14,6 +15,18 @@ public class Day1 {
       for(int i = 0; i < in.length(); i++) {
          if(in.charAt(i) == in.charAt((i + 1)%in.length())) {
             sum += Integer.parseInt(in.substring(i, i+1));
+         }
+      }
+      System.out.println(sum);
+   }
+
+   public static void a8(String in) {
+      ArrayList<Integer> list = new ArrayList<Integer>();
+      in.chars().forEach(i -> list.add(i - '0'));
+      int sum = 0;
+      for(int i = 0; i < list.size(); i++) {
+         if(list.get(i) == list.get((i + 1) % list.size())) {
+            sum += list.get(i);
          }
       }
       System.out.println(sum);
