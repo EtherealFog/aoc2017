d43f2fe77515a658317640d9094d5bd797c922ae
EtherealFog <baldwinelias77@gmail.com>
2017-12-03 21:11:03 -0800

    it's beginning to seem very unlikely that I make it into the top 100 for any of these problems. lol

--- day4/Day4.java
+++ day4/Day4.java
@@ -4,15 +4,50 @@ import java.io.*;
 public class Day4 {
    public static void main(String[] args) throws Exception {
       Scanner input = new Scanner(new File("input.txt"));
-      a();
-      b();
+      a(input);
+      input.close();
+      input = new Scanner(new File("input.txt"));
+      b(input);
    }
 
-   public static void a() {
-
+   public static void a(Scanner input) {
+      int sum = 0;
+      while(input.hasNextLine()) {
+         String line = input.nextLine();
+         Scanner linescan = new Scanner(line);
+         int num = 0;
+         Set<String> dupes = new HashSet<String>();
+         while(linescan.hasNext()) {
+            dupes.add(linescan.next());
+            num++;
+         }
+         if(num == dupes.size()) {
+            sum++;
+         }
+      }
+      System.out.println(sum);
    }
 
-   public static void b() {
-
+   public static void b(Scanner input) {
+      int sum = 0;
+      while(input.hasNextLine()) {
+         String line = input.nextLine();
+         Scanner linescan = new Scanner(line);
+         int num = 0;
+         Set<Set<Character>> dupes = new HashSet<Set<Character>>();
+         while(linescan.hasNext()) {
+            Set<Character> letters = new TreeSet<Character>();
+            String word = linescan.next();
+            for(char c: word.toCharArray()) {
+               letters.add(c);
+            }
+            dupes.add(letters);
+            num++;
+         }
+         if(num == dupes.size()) {
+            sum++;
+         }
+      }
+      System.out.println(sum);
    }
 }
