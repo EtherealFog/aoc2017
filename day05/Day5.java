b083db382bf3c6f341932547d7d3e197413fc816
EtherealFog <baldwinelias77@gmail.com>
2017-12-04 23:30:24 -0800

    Day 5 finished. pretty simple.

--- day05/Day5.java
+++ day05/Day5.java
@@ -1,16 +1,49 @@
 
-
+import java.util.*;
+import java.io.*;
 public class Day5 {
-   public static void main(String[] args) {
-      a();
-      b();
+   public static void main(String[] args) throws Exception{
+      Scanner input = new Scanner(new File("input.txt"));
+      List<Integer> listA = new ArrayList<Integer>();
+      while(input.hasNextLine()) {
+         listA.add(Integer.parseInt(input.nextLine()));
+      }
+      List<Integer> listB = copy(listA);
+      a(listA);
+      b(listB);
    }
 
-   public static void a() {
-
+   public static void a(List<Integer> input) {
+      int pos = 0;
+      int count = 0;
+      while(pos >= 0 && pos < input.size()) {
+         pos += input.set(pos, input.get(pos) + 1);
+         count++;
+      }
+      System.out.println(count);
    }
 
-   public static void b() {
+   public static void b(List<Integer> input) {
+      int pos = 0;
+      int count = 0;
+      while(pos >= 0 && pos < input.size()) {
+         int offset = input.get(pos);
+         if(offset > 2) {
+            offset--;
+         } else {
+            offset++;
+         }
+         pos += input.set(pos, offset);
+         count++;
+      }
+      System.out.println(count);
+   }
 
+   public static List<Integer> copy(List<Integer> list) {
+      List<Integer> result = new ArrayList<Integer>();
+      for(int i: list) {
+         result.add(i);
+      }
+      return result;
    }
 }
