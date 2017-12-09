0e19132a2b3547d224a1e8000895331f5a45b956
EtherealFog <baldwinelias77@gmail.com>
2017-12-08 21:38:10 -0800

    feel like I should have been able to do this faster

--- day09/Day9.java
+++ day09/Day9.java
@@ -1,16 +1,57 @@
 
-
+import java.util.*;
+import java.io.*;
 public class Day9 {
-   public static void main(String[] args) {
-      a();
-      b();
+   public static void main(String[] args) throws Exception {
+      Scanner in = new Scanner(new File("input.txt"));
+      StringBuilder input = new StringBuilder(in.nextLine());
+      new Day9(input);
    }
 
-   public static void a() {
-
+   public Day9(StringBuilder input) {
+      int garbage = 0;
+      int index = 0;
+      // remove all escape chars ('!') and escaped chars
+      while(index < input.length()) {
+         char curr = input.charAt(index);
+         if(curr == '!') {
+            input.deleteCharAt(index);
+            input.deleteCharAt(index);
+         } else {
+            index++;
+         }
+      }
+      // remove (and, for part B, count) all the garbage
+      index = 0;
+      while(index < input.length()) {
+         char curr = input.charAt(index);
+         if(curr == '<') {
+            while(input.charAt(index) != '>') {
+               input.deleteCharAt(index);
+               garbage++;
+            }
+            garbage--;
+            input.deleteCharAt(index);
+         }
+         index++;
+      }
+      index = 0;
+      System.out.println("Part A (Score): " + score(input));
+      System.out.println("Part B (Trash): " + garbage);
    }
 
-   public static void b() {
-
+   // find the score of a stream, provided garbage and escape chars have been removed.
+   public int score(StringBuilder input) {
+      int sum = 0;
+      int depth = 0;
+      for(char c: input.toString().toCharArray()) {
+         if(c == '{') {
+            depth++;
+         } else if(c == '}') {
+            sum += depth;
+            depth--;
+         }
+      }
+      return sum;
    }
 }
