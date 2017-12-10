ddcf68938b9f542ff1cb5e1eb5bf9afa1d0a4da8
EtherealFog <baldwinelias77@gmail.com>
2017-12-09 22:15:08 -0800

    whoops, a couple mistakes. cleaned it all up, commented. definitely works now

--- day10/Day10.java
+++ day10/Day10.java
@@ -14,46 +14,61 @@ public class Day10 {
    }
 
    public void a(String line) {
+      // Split up the input line on commas
       String[] vals = line.split(",");
+      // List to hold input
       List<Integer> nums = new ArrayList<Integer>();
       for(String s : vals) nums.add (Integer.parseInt(s));
+      // The 'string' that will be tied up in knots; vals 0 -> 255
       int[] string = new int[256];
       for(int i = 0; i < string.length; i++) string[i] = i;
+      // initialize skip and index vals
       int skip = 0;
       int ind = 0;
       for(int num: nums) {
          reverse(string, ind, num);
+         // wrap index around (circular string)
          ind = (ind + num + skip) % string.length;
+         // increment skip
          skip++;
       }
-      System.out.println("Part A Answer (product of first two elements): " + string[0]*string[1]);
+      System.out.println("Part A Answer (product of first two elements):\t" + string[0]*string[1]);
    }
 
    public void b(String line) {
+      // list for input vals
       List<Integer> nums = new LinkedList<Integer>();
+      // string array, all ints in [0, 255]
       int[] a = new int[256];
       for(int i = 0; i < a.length; i++) a[i] = i;
+      // for input: go through each character value to use as a 'length'
       for(char c: line.toCharArray()) nums.add((int)c);
+      // add all these arbitrary values designated in the problem
       nums.add(17); nums.add(31); nums.add(73); nums.add(47); nums.add(23);
+      // initialize skip and index vars
       int skip = 0;
       int ind = 0;
+      // basically do part A 64 times in a row, without resetting skip or index vals
       for(int r = 0; r < 64; r++) {
          for(int num: nums) {
             reverse(a, ind, num);
+            // wrap index around
             ind = (ind + num + skip) % a.length;
             skip++;
          }
       }
+      // XOR each of the 16 chunks of 16 vals together into one 16-length array
       int[] dense = condense(a);
-      System.out.println("Part B Answer (knot hash): " + result);
+      // make a string out of the hexadecimal representations of all the vals
+      System.out.println("Part B Answer (knot hash):\t\t\t" + hex(dense));
    }
 
    public static final String hex = "0123456789abcdef";
    public static String hex(int[] bytes) {
       String result = "";
       for(int i = 0; i < bytes.length; i++) {
-         result += hex.charAt(dense[i]/16);
-         result += hex.charAt(dense[i]%16);
+         result += hex.charAt(bytes[i]/16);
+         result += hex.charAt(bytes[i]%16);
       }
       return result;
    }
