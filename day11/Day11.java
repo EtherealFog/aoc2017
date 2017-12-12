7b9a91ba80f1c9df12a6585de4fc637b48700225
EtherealFog <baldwinelias77@gmail.com>
2017-12-11 23:15:28 -0800

    Day 11 and Day 12
    Looks like I forgot to commit yesterday. Oh well, here they both are. (got my best leaderboard result of 311th yesterday, btw).

--- day11/Day11.java
+++ day11/Day11.java
@@ -1,16 +1,45 @@
 
-
+import java.util.*;
+import java.io.*;
 public class Day11 {
-   public static void main(String[] args) {
-      a();
-      b();
+   public static void main(String[] args) throws Exception {
+      Scanner in = new Scanner(new File("input.txt"));
+      Scanner console = new Scanner(System.in);
+      String[] steps = in.nextLine().split(",");
+      Day11 a = new Day11();
+      a.run(steps);
    }
 
-   public static void a() {
-
+   public  void run(String[] steps) {
+      HexPoint p = new HexPoint();
+      int max = 0;
+      for(String s: steps) {
+         move(p, s);
+         int d = dist(p);
+         if(d > max) {
+            max = d;
+         }
+      }
+      System.out.println("Part A (Final Distance): " + dist(p));
+      System.out.println("Part B (Max Distance): " + max);
    }
 
-   public static void b() {
+   public int dist(HexPoint p) {
+      return (Math.abs(p.x) + Math.abs(p.y) + Math.abs(p.z)) / 2;
+   }
 
+   public void move(HexPoint p, String dir) {
+      switch(dir) {
+         case "n": p.y++; p.z--; return;
+         case "ne": p.x++; p.z--; return;
+         case "se": p.x++; p.y--; return;
+         case "s": p.y--; p.z++; return;
+         case "sw": p.x--; p.z++; return;
+         case "nw": p.x--; p.y++; return;
+      }
+   }
+
+   public class HexPoint {
+      public int x, y, z;
    }
 }
--- day12/Day12.java
+++ day12/Day12.java
@@ -1,16 +1,94 @@
 
-
+import java.util.*;
+import java.io.*;
 public class Day12 {
-   public static void main(String[] args) {
-      a();
-      b();
+   public static void main(String[] args) throws Exception{
+      Scanner in = new Scanner(new File("input.txt"));
+      Day12 d = new Day12();
+      d.build(in);
+      d.run();
    }
 
-   public static void a() {
+   public Map<Integer, Node> nodes;
 
+   public void build(Scanner in) {
+      nodes = new TreeMap<Integer, Node>();
+      while(in.hasNextLine()) {
+         String l = in.nextLine();
+         int n = Integer.parseInt(l.substring(0, l.indexOf(" ")));
+         String[] conns = l.substring(l.indexOf(">") + 2).split(", ");
+         if(!nodes.containsKey(n)) {
+            nodes.put(n, new Node(n));
+         }
+         Node node1 = nodes.get(n);
+         for(String s: conns) {
+            int nn = Integer.parseInt(s);
+            if(!nodes.containsKey(nn)) {
+               nodes.put(nn, new Node(nn));
+            }
+            Node node2 = nodes.get(nn);
+            node1.add(node2);
+         }
+      }
    }
 
-   public static void b() {
+   public Set<Set<Node>> groups;
 
+   public void numGroups() {
+      groups = new HashSet<Set<Node>>();
+      for(int n: nodes.keySet()) {
+         Node node = nodes.get(n);
+         if(!grouped(node)) {
+            Set<Node> newGroup = new HashSet<Node>();
+            node.connections(newGroup);
+            groups.add(newGroup);
+         }
+      }
+      System.out.println("Number of Groups:\t\t" + groups.size());
+   }
+
+   private boolean grouped(Node n) {
+      for(Set<Node> group: groups) {
+         if(group.contains(n)) return true;
+      }
+      return false;
+   }
+
+   public void run() {
+      // Part A
+      System.out.println("Connections in Node-0 Group:\t" + nodes.get(0).connections(new HashSet<Node>()));
+      // Part B
+      numGroups();
+   }
+
+   public class Node {
+      public int n;
+      public List<Node> pipes;
+      public Node(int n) {
+         pipes = new ArrayList<Node>();
+         this.n = n;
+      }
+
+      public String toString() {
+         return "" + n;
+      }
+
+      public void add(Node n) {
+         if(!pipes.contains(n)) {
+            pipes.add(n);
+            n.add(this);
+         }
+      }
+
+      public int connections(Set<Node> group) {
+         group.add(this);
+         int sum = 1;
+         for(Node n: pipes) {
+            if(!group.contains(n)) {
+               sum += n.connections(group);
+            }
+         }
+         return sum;
+      }
    }
 }
