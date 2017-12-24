78b89092d41f34d82d4370ed381e0290535a268e
EtherealFog <baldwinelias77@gmail.com>
2017-12-24 12:55:19 +0100

    lots and lots of cleaning up of Day24 code

--- day24/Day24.py
+++ day24/Day24.py
@@ -1,71 +1,56 @@
+import time
+# global variables
+strength_max = 0
+length_max = 0
+length_max_strength = 0
+components = set()
 
-max_strength = 0
-
-def a(lines):
-   components = []
+# parse the set of components from the lines from the input file
+def build(lines):
+   global components
    for line in lines:
       l = line.split("/")
       n1 = int(l[0])
       n2 = int(l[1])
-      ns = (min(n1, n2), max(n1, n2))
-      components.append(ns)
-   components.sort()
-   bridge_length(components)
-   return bridge(components)
+      ns = (n1, n2)
+      components.add(ns)
 
-def bridge(components, port=0, index=0, used=[]):
+# goes through
+def bridge(port=0, used=set()):
    found = False
-   strength = 0
-   for i in range(index, len(components)):
-      c = components[i]
-      if(c not in used):
-         if c[0] == port:
-            found = True
-            used.append(c)
-            strength = max(strength, bridge(components, c[1], 0, used))
-            used.remove(c)
-         elif c[1] == port:
-            found = True
-            used.append(c)
-            strength = max(strength, bridge(components, c[0], 0, used))
-            used.remove(c)
-   if not found:
+   global strength_max
+   global length_max
+   global length_max_strength
+   global components
+   # only consider components that have not already been used
+   for c in (components - used):
+      if c[0] == port:
+         found = True
+         used.add(c)
+         bridge(c[1], used)
+         used.remove(c)
+      elif c[1] == port:
+         found = True
+         used.add(c)
+         bridge(c[0], used)
+         used.remove(c)
+   if not found: # i.e. no more possible additions to the bridge
+      # add up strength of components used to make this bridge
       s = 0
       for c in used:
          s += c[0] + c[1]
-      strength = max(s, strength)
-   return strength
-
-length = 0
-len_max = 0
-def bridge_length(components, port=0, index=0, used=[]):
-   found = False
-   global length
-   global len_max
-   for i in range(index, len(components)):
-      c = components[i]
-      if(c not in used):
-         if c[0] == port:
-            found = True
-            used.append(c)
-            length = max(length, bridge_length(components, c[1], 0, used))
-            used.remove(c)
-         elif c[1] == port:
-            found = True
-            used.append(c)
-            length = max(length, bridge_length(components, c[0], 0, used))
-            used.remove(c)
-   if not found:
-      s = 0
-      for c in used:
-         s += c[0] + c[1]
-      if(len(used) >= length):
-         len_max = max(s, len_max)
-      length = max(len(used), length)
-   return length
-
+      # reassign strength_max if new maximum strength has been found
+      strength_max = max(s, strength_max)
+      # reassign length_strength_max if new maximum strength has been found for the current max length
+      if(len(used) >= length_max):
+         length_max_strength = max(s, length_max_strength)
+      length_max = max(len(used), length_max)
+    
 with open("input.txt") as f:
-   lines = f.readlines()
-   print("Part A:", a(lines)) 
-   print("Part B:", len_max)
+   t = time.time()
+   build(f.readlines())
+   bridge()
+   print("Took " + str(round(time.time()-t, 3)) + " seconds to find:")
+   print("Part A: " + str(strength_max)) 
+   print("Part B: " + str(length_max_strength))
    
